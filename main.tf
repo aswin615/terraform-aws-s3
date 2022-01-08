4b4490d290f0bb9ccafdc0625cf42c2ec9d6405e
aswin615 <aswin.study@gmail.com>
2022-01-08 21:40:23 +0530

    s3 creation

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = var.region
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning = {
+    enabled = var.versioning
+  }
+
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default = "us-east-1"
+  
+}
+variable "versioning" {
+    type = string
+    default = "true" 
+}
+
+variable "bucket_name" {
+    type = string
+    default = "demostate-file-bucket"  
+}
