fe793e0e4bfb64c45d0f430cfd46be2ab9cf99fc
Aswin Vins C <aswin.vins-c@capgemini.com>
2022-06-18 10:10:44 +0530

    test

--- main.tf
+++ main.tf
@@ -53,3 +53,16 @@ module "s3_bucket" {
   }
 
 }
+
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning = {
+    enabled = var.versioning
+  }
+
+}
