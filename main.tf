5b7cd34f1230a1d8fb6d77b417f9760d9d61ce64
aswin615 <89241594+aswin615@users.noreply.github.com>
2023-05-07 12:55:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,44 +15,10 @@ module "s3_bucket" {
 }
 
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = var.bucket_name
-  acl    = "private"
-
-  versioning = {
-    enabled = var.versioning
-  }
-
-}
-
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = var.bucket_name
-  acl    = "private"
-
-  versioning = {
-    enabled = var.versioning
-  }
-
-}
 
 
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.bucket_name
-  acl    = "private"
-
-  versioning = {
-    enabled = var.versioning
-  }
-
-}
 
 
 module "s3_bucket" {
